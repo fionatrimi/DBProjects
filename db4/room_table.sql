0c1ac1d602a42bb9b64017b47a11963a6aaa6457
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-05-02 21:13:48 +0300

    add comments

--- db4/room_table.sql
+++ db4/room_table.sql
@@ -13,14 +13,22 @@ ALTER TABLE temp_room
 ADD FOREIGN KEY (id) REFERENCES temp_listings(id);
 
 ALTER TABLE temp_listings
-DROP accommodates,
-DROP bathrooms,
-DROP bedrooms,
-DROP beds,
-DROP bed_type,
-DROP amenities,
-DROP square_feet,
-DROP price,
-DROP weekly_price, 
-DROP monthly_price,
-DROP security_deposit;
+    DROP accommodates,
+    DROP bathrooms,
+    DROP bedrooms,
+    DROP beds,
+    DROP bed_type,
+    DROP amenities,
+    DROP square_feet,
+    /*
+	PROSOXH OI STHLES price, weekly_price, monthly_price, security_deposit
+	DIAGRAFONTAI KAI APO TO ARXEIO price_table.sql
+	-EKSARTHSH APO THN SEIRA POY TREXEI TO KATHE ARXEIO
+	AN TREKSOUME TO price_table.sql PRIN TO room_table.sql
+	TOTE NA MPOUN SE SXOLIO OI PARAKATW 4 GRAMMES
+	*/
+    DROP price,
+    DROP weekly_price, 
+    DROP monthly_price,
+    DROP security_deposit
+    ;
