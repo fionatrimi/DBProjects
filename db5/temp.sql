cb683621779ffefd490beb19dc8f6e4e02c8cafd
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-11 19:43:49 +0300

    Update temp.sql

--- db5/temp.sql
+++ db5/temp.sql
@@ -21,3 +21,16 @@ ALTER TABLE tempAmenity
 ADD COLUMN amenity_id SERIAL PRIMARY KEY;			       
 												 
                   
+CREATE TABLE "Room_Amenities_Temp"(
+	room_id int,
+	amenity_id int
+);
+
+WITH cte_room_amenities(room_id,amenity_name) as(
+    SELECT id as room_id, unnest(string_to_array(amenities,',')) as amenity_name 
+	from "Room" as r
+)
+INSERT INTO "Room_Amenities_Temp"
+SELECT room_id , tempAmenity2.amenity_id
+	FROM cte_room_amenities,tempAmenity2
+	WHERE cte_room_amenities.amenity_name = tempAmenity2.amenity_name;
