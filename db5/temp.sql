be0e617056b072bb00db21b2699c238bc8f326fc
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-11 18:54:42 +0300

    Update temp.sql

--- db5/temp.sql
+++ db5/temp.sql
@@ -1,1 +1,20 @@
-test
+UPDATE "Room"
+SET
+amenities = REPLACE(amenities,'{','');
+
+UPDATE "Room"
+SET
+amenities = REPLACE(amenities,'}','');
+
+
+CREATE TABLE tempAmenity AS
+(	SELECT DISTINCT unnest(string_to_array(amenities,',')) as amenity_name
+	FROM "Room"
+);
+                         
+                        
+UPDATE tempAmenity
+SET
+amenity_name = REPLACE (amenity_name,'"','');                         
+												 
+                  
