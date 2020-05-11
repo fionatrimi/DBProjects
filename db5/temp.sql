af5e998094a47d55f36de0627e3844ae1edc65c8
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-11 20:11:35 +0300

    Update temp.sql

--- db5/temp.sql
+++ db5/temp.sql
@@ -27,6 +27,13 @@ CREATE TABLE "Room_Amenities_Temp"(
 	PRIMARY KEY (room_id,amenity_id)
 );
 
+ALTER TABLE "Room_Amenities_Temp"
+ADD FOREIGN KEY (room_id) REFERENCES"Room"(id);	
+			       
+
+ALTER TABLE "Room_Amenities_Temp"
+ADD FOREIGN KEY (amenity_id) REFERENCES tempAmenity(amenity_id)	;		       
+
 WITH cte_room_amenities(room_id,amenity_name) as(
     SELECT id as room_id, unnest(string_to_array(amenities,',')) as amenity_name 
 	from "Room" as r
