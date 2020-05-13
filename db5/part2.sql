c8842a5b67c947408323e58861c48c3b05848e92
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-13 18:31:09 +0300

    Create part2.sql

---
+++ db5/part2.sql
@@ -0,0 +1,7 @@
+Εμφανιζει για καθε room τα amenities του
+
+SELECT r.id,am.amenity_name
+FROM "Room" as r
+INNER JOIN "Room_Amenities" as ra ON ra.room_id = r.id
+INNER JOIN "Amenity" as am ON am.amenity_id = ra.amenity_id
+ORDER BY(r.id)
