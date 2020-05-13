37f506461b4502a6b2c74ce8381fda91bdf61c2d
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-05-13 19:09:08 +0300

    Update part2.sql

--- db5/part2.sql
+++ db5/part2.sql
@@ -14,3 +14,10 @@ INNER JOIN "Amenity" AS am ON am.amenity_id = ra.amenity_id
 WHERE am.amenity_name IN ('Dishwasher','Dryer')
 GROUP BY r.id
 HAVING COUNT(am.amenity_name)=2
+
+--Υπηρεσιες που διατιθενται σε παραπανω απο 10 δωματια
+SELECT COUNT(r_a.room_id), am.amenity_name
+FROM
+("Room_Amenities" r_a INNER JOIN "Amenity" am ON r_a.amenity_id = am.amenity_id)
+GROUP BY am.amenity_name
+HAVING COUNT(r_a.room_id) > 100
