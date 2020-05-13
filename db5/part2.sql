838fa3df122650cfde50cd813c0b5d693caa4e27
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-13 19:05:27 +0300

    Update part2.sql

--- db5/part2.sql
+++ db5/part2.sql
@@ -5,3 +5,12 @@ FROM "Room" as r
 INNER JOIN "Room_Amenities" as ra ON ra.room_id = r.id
 INNER JOIN "Amenity" as am ON am.amenity_id = ra.amenity_id
 ORDER BY(r.id)
+
+
+Select r.id
+FROM "Room" as r
+INNER JOIN "Room_Amenities" AS ra ON ra.room_id = r.id
+INNER JOIN "Amenity" AS am ON am.amenity_id = ra.amenity_id
+WHERE am.amenity_name IN ('Dishwasher','Dryer')
+GROUP BY r.id
+HAVING COUNT(am.amenity_name)=2
