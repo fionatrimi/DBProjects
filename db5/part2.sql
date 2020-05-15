0d13a173a8fe5bbd306a000afa32461f749c4714
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-15 18:21:07 +0300

    Update part2.sql

--- db5/part2.sql
+++ db5/part2.sql
@@ -49,7 +49,7 @@ ON neig.neighbourhood = loc.neighbourhood_cleansed
 INNER JOIN "Geolocation" AS geo
 ON geo.properties_neighbourhood=neig.neighbourhood
 
---Εμφανίζει τα ονόματα των host που διαθέτουν δωμάτια με 3 κρεβάτια, 3 άτομα και απαντάνε εντώς μιας ώρας
+--Εμφανίζει τα ονόματα των host και τον αριθμο των δωματιων που διαθέτουν(εκανα αυτη την αλλαγη για να χρησιμποιησουμε ακομα 1 group by) με 3 κρεβάτια, 3 άτομα και απαντάνε εντώς μιας ώρας
 SELECT h.name, h.id
 FROM
 	(SELECT li.host_id
@@ -64,9 +64,10 @@ ON h.id = host_ID.host_id
 WHERE h.response_time='within an hour'
 
 // αυτο ειναι το ιδιο με το παραπανω απλα χωρις subquery select. Νομιζω γενικα μπορουμε να τα αποφυγουμε δω τα subqueries//
-SELECT h.name,h.id
+SELECT h.name,h.id,COUNT(h.id)
 	FROM "Host" as h
 	INNER JOIN "Listings" as l ON l.host_id = h.id
 	INNER JOIN "Room" as r ON r.id = l.id 
 	INNER JOIN "Room_Amenities" as ra ON ra.room_id = r.id
 	WHERE r.beds=3 AND r.accommodates=3 AND h.response_time = 'within an hour'
+	GROUP BY (h.id)
