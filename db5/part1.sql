2dac27679aa83a9640ce281b3edfec2d85dfbf15
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-17 17:57:49 +0300

    Update part1.sql

--- db5/part1.sql
+++ db5/part1.sql
@@ -7,21 +7,21 @@ SET
 amenities = REPLACE(amenities,'}','');
 
 
-CREATE TABLE tempAmenity AS
+CREATE TABLE "Amenity" AS
 (	SELECT DISTINCT unnest(string_to_array(amenities,',')) as amenity_name
 	FROM "Room"
 );
                          
                         
-UPDATE tempAmenity
+UPDATE "Amenity"
 SET
 amenity_name = REPLACE (amenity_name,'"','');
 			       
-ALTER TABLE tempAmenity
+ALTER TABLE "Amenity"
 ADD COLUMN amenity_id SERIAL PRIMARY KEY;			       
 												 
                   
-CREATE TABLE "Room_Amenities_Temp"(
+CREATE TABLE "Room_Amenities"(
 	room_id int,
 	amenity_id int,
 	PRIMARY KEY (room_id,amenity_id)
@@ -31,17 +31,17 @@ WITH cte_room_amenities(room_id,amenity_name) as(
     SELECT id as room_id, unnest(string_to_array(amenities,',')) as amenity_name 
 	from "Room" as r
 )
-INSERT INTO "Room_Amenities_Temp"
-SELECT DISTINCT room_id , tempAmenity.amenity_id
-	FROM cte_room_amenities,tempAmenity
-	WHERE cte_room_amenities.amenity_name = tempAmenity.amenity_name;
+INSERT INTO "Room_Amenities"
+SELECT DISTINCT room_id , "Amenity".amenity_id
+	FROM cte_room_amenities,"Amenity"
+	WHERE cte_room_amenities.amenity_name = "Amenity".amenity_name;
 				 
 ALTER TABLE "Room"
 ADD PRIMARY KEY (id);				 
 
-ALTER TABLE "Room_Amenities_Temp"
-ADD FOREIGN KEY (room_id) REFERENCES"Room"(id);	
+ALTER TABLE "Room_Amenities"
+ADD FOREIGN KEY (room_id) REFERENCES "Room"(id);	
 			       
 
-ALTER TABLE "Room_Amenities_Temp"
-ADD FOREIGN KEY (amenity_id) REFERENCES tempAmenity(amenity_id)	;					 
+ALTER TABLE "Room_Amenities"
+ADD FOREIGN KEY (amenity_id) REFERENCES "Amenity"(amenity_id)	;					 
