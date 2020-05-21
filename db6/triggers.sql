120697398b71d695173687c5a86a4df7da2b652c
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-05-22 01:05:11 +0300

    Part A

---
+++ db6/triggers.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION set_host_listings()
+RETURNS TRIGGER AS
+'BEGIN
+	IF TG_OP = ''INSERT'' THEN
+		UPDATE "Host_6"
+		SET listings_count=listings_count+1
+		WHERE id=NEW.id;
+		RETURN NEW;
+	ELSIF TG_OP = ''DELETE'' THEN
+		UPDATE "Host_6"
+		SET listings_count=listings_count-1
+		WHERE id=OLD.id;
+		RETURN OLD;
+	END IF;
+	--return null;
+END;'
+LANGUAGE plpgsql;
+
+CREATE TRIGGER setter_host_listings AFTER INSERT OR DELETE
+ON "Listing_6"
+FOR EACH ROW
+EXECUTE PROCEDURE set_host_listings();
