7d9601173df1bb18733825dacd4ae402b1bc5669
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-05-25 15:10:43 +0300

    Added 2nd way

--- db6/triggers.sql
+++ db6/triggers.sql
@@ -20,3 +20,35 @@ CREATE TRIGGER setter_host_listings AFTER INSERT OR DELETE
 ON "Listing_6"
 FOR EACH ROW
 EXECUTE PROCEDURE set_host_listings();
+
+------------------------------2nd WAY----------------------------------------------
+CREATE FUNCTION incr_host_listings()
+RETURNS TRIGGER AS
+'BEGIN
+		UPDATE "Host_6b"
+		SET host_listings_count=host_listings_count+1
+		WHERE id=NEW.id;
+		RETURN NEW;
+END;'
+LANGUAGE plpgsql;
+
+CREATE TRIGGER incr_count AFTER INSERT
+ON "Listing_6b"
+FOR EACH ROW
+EXECUTE PROCEDURE incr_host_listings();
+
+
+CREATE FUNCTION decr_host_listings()
+RETURNS TRIGGER AS
+'BEGIN
+		UPDATE "Host_6b"
+		SET host_listings_count=host_listings_count-1
+		WHERE id=OLD.id;
+		RETURN OLD;
+END;'
+LANGUAGE plpgsql;
+
+CREATE TRIGGER decr_count AFTER DELETE
+ON "Listing_6b"
+FOR EACH ROW
+EXECUTE PROCEDURE decr_host_listings();
