0722cb21940606ad7ccad8e259cbd9bead4cfa4b
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-05-27 02:48:06 +0300

    Create  query_indexes.sql

---
+++ db6/query_indexes.sql
@@ -0,0 +1,7 @@
+/* Query: 
+  explain analyze SELECT "Host".id, COUNT(*) FROM "Listing", "Host" WHERE "Host".id="Listing".host_id GROUP BY "Host".id;  */
+create index IX_hosts_id on "Listing" (host_id);
+ 
+ /* Query:
+    explain analyze SELECT L.id, price FROM "Listing" L, "Price" P WHERE guests_included > 5 AND price > 40; */
+create index IX_prices_guests on "Price" (id, price, guests_included);
