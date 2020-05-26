7dd5bd6d2bfa4663d80fad5b2fc3fdcb358f6dcc
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-26 18:41:45 +0300

    Update triggers.sql

--- db6/triggers.sql
+++ db6/triggers.sql
@@ -22,6 +22,49 @@ ON "Listing_6"
 FOR EACH ROW
 EXECUTE PROCEDURE set_host_listings();
 
+CREATE OR REPLACE FUNCTION set_host_listings()
+RETURNS TRIGGER AS $emp$
+BEGIN
+	IF TG_OP = 'INSERT' THEN
+		UPDATE "Host"
+		SET host_listings_count=host_listings_count+1
+		WHERE NEW.host_id = "Host".id;
+		RETURN NEW;
+	ELSIF TG_OP = 'DELETE' THEN
+		UPDATE "Host"
+		SET host_listings_count=host_listings_count-1
+		WHERE OLD.host_id = "Host".id;
+		RETURN OLD;
+	END IF;
+	--return null;
+END;
+$emp$
+LANGUAGE plpgsql
+
+CREATE OR REPLACE FUNCTION update_number_of_reviews()
+RETURNS TRIGGER AS $emp2$
+BEGIN
+	IF TG_OP = 'INSERT' THEN
+		UPDATE "Listings"
+		SET number_of_reviews = number_of_reviews + 1
+		WHERE NEW.listing_id = "Listings".id; 
+		RETURN NEW;
+	ELSIF TG_OP = 'DELETE' THEN
+		UPDATE "Listings"
+		SET number_of_reviews = number_of_reviews - 1
+		WHERE OLD.listing_id = "Listings".id;
+		RETURN OLD;
+	END IF;
+	--return null;
+END;
+$emp2$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_delete_reviews_trigger AFTER INSERT OR DELETE
+ON "Reviews"
+FOR EACH ROW
+EXECUTE PROCEDURE update_number_of_reviews();
+
 ------------------------------2nd WAY----------------------------------------------
 CREATE FUNCTION incr_host_listings()
 RETURNS TRIGGER AS
