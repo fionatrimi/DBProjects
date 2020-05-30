28ea0e23cbeb066240a100509a8c7e8c46c6f60f
Hercux <59443048+Hercux@users.noreply.github.com>
2020-05-30 20:16:35 +0300

    Update query_indexes.sql

--- db6/query_indexes.sql
+++ db6/query_indexes.sql
@@ -5,3 +5,6 @@ create index IX_hosts_id on "Listing" (host_id);
  /* Query:
     explain analyze SELECT L.id, price FROM "Listing" L, "Price" P WHERE guests_included > 5 AND price > 40; */
 create index IX_prices_guests on "Price" (id, price, guests_included);
+
+/* Query 5 */
+CREATE INDEX IX_room_beds_accommodates ON "Room"(beds,accommodates);
