a81b100df5a69d300bcea94bdff962bbf0c05dee
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-06-01 18:33:45 +0300

    Update query_indexes.sql

--- db6/query_indexes.sql
+++ db6/query_indexes.sql
@@ -1,13 +1,15 @@
 /* Query: 
   explain analyze SELECT "Host".id, COUNT(*) FROM "Listing", "Host" WHERE "Host".id="Listing".host_id GROUP BY "Host".id;  */
 create index IX_hosts_id on "Listing" (host_id);
+--drop index ix_hosts_id
  
  /* Query:
     explain analyze SELECT L.id, price FROM "Listing" L, "Price" P WHERE guests_included > 5 AND price > 40; */
 create index IX_prices_guests on "Price" (id, price, guests_included);
 
 /* Query 4 */
-CREATE INDEX IX_room_amenities_amenity_od ON "Room_Amenities" (amenity_id);
+CREATE INDEX IX_room_amenities_amenity_id ON "Room_Amenities" (amenity_id);
+--drop index IX_room_amenities_amenity_id
 
 /* Query 5 */
 CREATE INDEX IX_room_beds_accommodates ON "Room"(beds,accommodates);
