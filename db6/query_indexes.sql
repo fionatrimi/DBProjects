86fc3b1970f8a7f15756debd1b8909723bd953ef
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-06-01 20:26:53 +0300

    Update query_indexes.sql

--- db6/query_indexes.sql
+++ db6/query_indexes.sql
@@ -1,12 +1,12 @@
 /* Query (Prwto QUERY ekfwnishs): 
   explain analyze SELECT "Host".id, COUNT(*) FROM "Listing", "Host" WHERE "Host".id="Listing".host_id GROUP BY "Host".id;  */
 create index IX_hosts_id on "Listing" (host_id);
---drop index ix_hosts_id
+--drop index ix_hosts_id;
  
  /* Query (Deftero QUERY ekfwnishs):
     explain analyze SELECT L.id, price FROM "Listing" L, "Price" P WHERE guests_included > 5 AND price > 40; */
 create index IX_prices_guests on "Price" (id, price, guests_included);
---drop index IX_prices_guests
+--drop index IX_prices_guests;
 
 
 ---------QUERIES APO THN 5h ERGASIA------------------
@@ -17,8 +17,8 @@ create index IX_prices_guests on "Price" (id, price, guests_included);
 /*Query 2 */
 CREATE INDEX IX_price ON "Price" (price);
 CREATE INDEX IX_room_amenities_amenity_id ON "Room_Amenities" (amenity_id);
---drop index IX_price
---drop index IX_room_amenities_amenity_id
+--drop index IX_price;
+--drop index IX_room_amenities_amenity_id;
 
 /*Query 3 */
 --Den iparxoyn indexes poy thn epitaxinoun
@@ -26,7 +26,8 @@ CREATE INDEX IX_room_amenities_amenity_id ON "Room_Amenities" (amenity_id);
 
 /* Query 4 */
 CREATE INDEX IX_room_amenities_amenity_id ON "Room_Amenities" (amenity_id);
---drop index IX_room_amenities_amenity_id
+--drop index IX_room_amenities_amenity_id;
 
 /* Query 5 */
 CREATE INDEX IX_room_beds_accommodates ON "Room"(beds,accommodates);
+--drop index IX_room_beds_accommodates;
