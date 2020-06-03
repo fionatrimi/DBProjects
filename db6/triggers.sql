a191beff7380ba4987adfe0cc1bc2e17dd5289a3
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-06-03 21:34:39 +0300

    Update triggers.sql

--- db6/triggers.sql
+++ db6/triggers.sql
@@ -1,26 +1,4 @@
-CREATE OR REPLACE FUNCTION set_host_listings()
-RETURNS TRIGGER AS $emp$
-BEGIN
-	IF TG_OP = 'INSERT' THEN
-		UPDATE "Host"
-		SET host_listings_count=host_listings_count+1
-		WHERE NEW.host_id = "Host".id;
-		RETURN NEW;
-	ELSIF TG_OP = 'DELETE' THEN
-		UPDATE "Host"
-		SET host_listings_count=host_listings_count-1
-		WHERE OLD.host_id = "Host".id;
-		RETURN OLD;
-	END IF;
-	--return null;
-END;
-$emp$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER setter_host_listings AFTER INSERT OR DELETE
-ON "Listing_6"
-FOR EACH ROW
-EXECUTE PROCEDURE set_host_listings();
+--TRIGGER EKFWNHSHS
 
 CREATE OR REPLACE FUNCTION set_host_listings()
 RETURNS TRIGGER AS $emp$
@@ -41,18 +19,25 @@ END;
 $emp$
 LANGUAGE plpgsql
 
+CREATE TRIGGER setter_host_listings AFTER INSERT OR DELETE
+ON "Listing"
+FOR EACH ROW
+EXECUTE PROCEDURE set_host_listings();
+
+--DIKO MAS TRIGGER, UPDATES NUMBER OF REVIEWS OF A LISTING
+
 CREATE OR REPLACE FUNCTION update_number_of_reviews()
 RETURNS TRIGGER AS $emp2$
 BEGIN
 	IF TG_OP = 'INSERT' THEN
-		UPDATE "Listings"
+		UPDATE "Listing"
 		SET number_of_reviews = number_of_reviews + 1
-		WHERE NEW.listing_id = "Listings".id; 
+		WHERE NEW.listing_id = "Listing".id; 
 		RETURN NEW;
 	ELSIF TG_OP = 'DELETE' THEN
-		UPDATE "Listings"
+		UPDATE "Listing"
 		SET number_of_reviews = number_of_reviews - 1
-		WHERE OLD.listing_id = "Listings".id;
+		WHERE OLD.listing_id = "Listing".id;
 		RETURN OLD;
 	END IF;
 	--return null;
@@ -61,38 +46,7 @@ $emp2$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER insert_delete_reviews_trigger AFTER INSERT OR DELETE
-ON "Reviews"
+ON "Review"
 FOR EACH ROW
 EXECUTE PROCEDURE update_number_of_reviews();
 
-------------------------------2nd WAY----------------------------------------------
-CREATE FUNCTION incr_host_listings()
-RETURNS TRIGGER AS
-'BEGIN
-		UPDATE "Host_6b"
-		SET host_listings_count=host_listings_count+1
-		WHERE id=NEW.id;
-		RETURN NEW;
-END;'
-LANGUAGE plpgsql;
-
-CREATE TRIGGER incr_count AFTER INSERT
-ON "Listing_6b"
-FOR EACH ROW
-EXECUTE PROCEDURE incr_host_listings();
-
-
-CREATE FUNCTION decr_host_listings()
-RETURNS TRIGGER AS
-'BEGIN
-		UPDATE "Host_6b"
-		SET host_listings_count=host_listings_count-1
-		WHERE id=OLD.id;
-		RETURN OLD;
-END;'
-LANGUAGE plpgsql;
-
-CREATE TRIGGER decr_count AFTER DELETE
-ON "Listing_6b"
-FOR EACH ROW
-EXECUTE PROCEDURE decr_host_listings();
