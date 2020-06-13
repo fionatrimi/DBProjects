f7493dcb1c528049a4db7fe6a1b5c0753dc89dd9
Hercux <59443048+Hercux@users.noreply.github.com>
2020-06-13 18:06:36 +0300

    Update duplicates.sql

--- project/duplicates.sql
+++ project/duplicates.sql
@@ -20,3 +20,33 @@ SET
 keywords = k.keywords
 FROM "Movies".Keywords as k
 WHERE k2.id = k.id
+
+
+UPDATE "Movies".Movies_Metadata2 as md2
+SET
+adult  = md.adult ,
+belongs_to_collection  = md.belongs_to_collection,
+budget   = md.budget  ,
+genres   = md.genres ,
+homepage   = md.homepage  ,
+imdb_id   = md.imdb_id ,
+original_language   = md.original_language  ,
+original_title   = md.original_title ,
+overview   = md.overview  ,
+popularity   = md.popularity ,
+poster_path   = md.poster_path  ,
+production_companies   = md.production_companies ,
+production_countries    = md.production_countries  ,
+release_date    = md.release_date   ,
+revenue    = md.revenue  ,
+runtime    = md.runtime  ,
+spoken_languages    = md.spoken_languages   ,
+status    = md.status  ,
+tagline     = md.tagline   ,
+title     = md.title    ,
+video     = md.video   ,
+vote_average      = md.vote_average     ,
+vote_count      = md.vote_count    
+
+FROM "Movies".Movies_Metadata as md
+WHERE md2.id = md.id;
