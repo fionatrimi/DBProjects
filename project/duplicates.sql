9a87cab79415c511a09d7f546746b53a330b63ea
Hercux <59443048+Hercux@users.noreply.github.com>
2020-06-13 17:31:27 +0300

    Update duplicates.sql

--- project/duplicates.sql
+++ project/duplicates.sql
@@ -1,3 +1,10 @@
 INSERT INTO "Movies".Credits2(id)
 SELECT DISTINCT id
 	FROM "Movies".Credits
+
+UPDATE "Movies".Credits2 as c2
+SET
+casting = c.casting,
+crew = c.crew
+FROM "Movies".Credits as c
+WHERE c2.id = c.id
