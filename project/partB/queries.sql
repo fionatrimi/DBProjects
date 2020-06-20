5d7d9e3170f01f0cb8d2101a24d2b323f8ca0815
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-06-20 21:01:21 +0300

    Update queries.sql

--- project/partB/queries.sql
+++ project/partB/queries.sql
@@ -1,11 +1,18 @@
-/* Αριθμός από ratings ανά χρήστη */
-select userid, count(userid)
-from "Movies".ratings_small
-group by userid;
 
-/*Αριθμός ταινιών ανά είδος(genres)*/
+/* Αριθμός ταινιών ανά είδος(genres) */
 SELECT
 y.x->'name' "name",COUNT(id)
 FROM "Movies".movies_metadata2,
 LATERAL (SELECT jsonb_array_elements(genres::jsonb)x)y
 GROUP BY y.x
+
+/* Αριθμός από ratings ανά χρήστη */
+select userid, count(userid)
+from "Movies".ratings_small
+group by userid;
+
+/* Μέση βαθμολογία (rating) ανά χρήστη */
+select userid, 
+round(avg(rating::numeric), 2) as avg_rating_per_user
+from "Movies".ratings_small
+group by userid
