a31216528fd02184d1f80944fd67d56dd654783a
Hercux <59443048+Hercux@users.noreply.github.com>
2020-06-20 21:04:49 +0300

    Update queries.sql

--- project/partB/queries.sql
+++ project/partB/queries.sql
@@ -11,6 +11,15 @@ select userid, count(userid)
 from "Movies".ratings_small
 group by userid;
 
+
+/*Μέση βαθμολογία (rating) ανά είδος (ταινίας)*/
+SELECT
+y.x->'name' "name", round(avg(rating::numeric), 2)
+from "Movies".movies_metadata2 as mm2
+INNER JOIN "Movies".ratings_small as r ON r.movieid = mm2.id,
+LATERAL (SELECT jsonb_array_elements(genres::jsonb)x)y
+GROUP BY y.x
+
 /* Μέση βαθμολογία (rating) ανά χρήστη */
 select userid, 
 round(avg(rating::numeric), 2) as avg_rating_per_user
