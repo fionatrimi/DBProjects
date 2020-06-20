dc5ccbce740cb9860046d3c3732c60839fcea6c4
Hercux <59443048+Hercux@users.noreply.github.com>
2020-06-20 20:50:32 +0300

    Update queries.sql

--- project/partB/queries.sql
+++ project/partB/queries.sql
@@ -2,3 +2,10 @@
 select userid, count(userid)
 from "Movies".ratings_small
 group by userid;
+
+/*Αριθμός ταινιών ανά είδος(genres)*/
+SELECT
+y.x->'name' "name",COUNT(id)
+FROM "Movies".movies_metadata2,
+LATERAL (SELECT jsonb_array_elements(genres::jsonb)x)y
+GROUP BY y.x
